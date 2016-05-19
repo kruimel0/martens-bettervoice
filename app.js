a3be736b5baeb0687810976a061d1971959830c6
Matjaž Lipuš <matjazl@gmail.com>
2016-05-19 22:43:01 +0200

    DRY

--- app.js
+++ app.js
@@ -23,13 +23,7 @@ Homey.manager('flow').on('condition.voice_output', function( callback, args ){
 	var randval=arr_t[Math.floor(Math.random()*(arr_t.length))];//Get a random entry from (0 - array_length)
 	Homey.manager('speech-input').ask(randval, function( err, result ) {
         if( err ) return Homey.error(err);
-		if (result == 'yes'){
-			callback( null, true );
-		}
-		if (result == 'no'){
-			callback( null, false );
-		}
-        // result is the transcript of the user, in this case it might be `Mike`
+	callback( null, result == 'yes' );
     });
 	
 });
